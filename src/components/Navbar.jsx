f86857884fdb7c3ff0bc82518ab57ce7067c5f21
Jason Showalter <jyshowalter@gmail.com>
2024-06-18 23:52:29 -0700

    added navbar and carousel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,102 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
-  const linkClass = ({ isActive }) =>
-    isActive
-      ? " bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-      : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
   return (
-    <nav className="bg-indigo-700 border-b border-indigo-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            {/* <!-- Logo --> */}
-            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
-              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                React Jobs
-              </span>
-            </NavLink>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <NavLink to="/" className={linkClass}>
-                  Home
-                </NavLink>
-                <NavLink to="/jobs" className={linkClass}>
-                  Jobs
-                </NavLink>
-                <NavLink to="/add-job" className={linkClass}>
-                  Add Job
-                </NavLink>
-              </div>
-            </div>
+    <div className="navbar bg-base-100 my-4 ml-4 font-raleway">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>HOME</a>
+            </li>
+            <li>
+              <a>PORTFOLIO</a>
+              <ul className="p-2">
+                <li>
+                  <a>WEDDING</a>
+                </li>
+                <li>
+                  <a>ENGAGEMENT</a>
+                </li>
+                <li>
+                  <a>FAMILY PORTRAITS</a>
+                </li>
+                <li>
+                  <a>SENIOR/GRADUATION</a>
+                </li>
+                <li>
+                  <a>MATERNITY</a>
+                </li>
+                <li>
+                  <a>NATURE</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>ABOUT ME</a>
+            </li>
+          </ul>
         </div>
+        <a className="btn btn-ghost text-xl">Prieto Photography</a>
       </div>
-    </nav>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 font-semibold">
+          <li>
+            <a>HOME</a>
+          </li>
+          <li>
+            <details>
+              <summary>PORTFOLIO</summary>
+              <ul className="p-2">
+                <li>
+                  <a>WEDDING</a>
+                </li>
+                <li>
+                  <a>ENGAGEMENT</a>
+                </li>
+                <li>
+                  <a>FAMILY PORTRAITS</a>
+                </li>
+                <li>
+                  <a>SENIOR/GRADUATION</a>
+                </li>
+                <li>
+                  <a>MATERNITY</a>
+                </li>
+                <li>
+                  <a>NATURE</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>ABOUT ME</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end mr-8">
+        <a className="btn">CONTACT</a>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif"],
+        raleway: ["Raleway"],
+        playwrite: ["Playwrite Portugal"],
       },
       gridTemplateColumns: {
         "70/30": "70% 28%",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["autumn"],
+  },
 };
